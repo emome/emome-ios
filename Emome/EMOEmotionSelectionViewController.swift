87920b06db5b12d2613411b1af7e35252236f0da
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2015-09-28 11:09:30 -0400

    Add alert as a placeholder for the activity list.

--- Emome/EMOEmotionSelectionViewController.swift
+++ Emome/EMOEmotionSelectionViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EMOEmotionSelectionViewController: UIViewController {
+class EMOEmotionSelectionViewController: UIViewController, UIGestureRecognizerDelegate {
     
     let emotions:[String] = ["happy", "angry", "lonely", "excited", "sad"]
     let emotionColors:[UIColor] = [
@@ -40,16 +40,25 @@ class EMOEmotionSelectionViewController: UIViewController {
             emotionPage.backgroundColor = self.emotionColors[i]
             self.emotionPages.addSubview(emotionPage)
             
-            let emotionLabel = UILabel(frame: CGRectMake(32.0, 200.0, 320.0, 100.0))
+            let emotionLabel = UILabel(frame: CGRectMake(0.0, 0.0, 320.0, 100.0))
             emotionLabel.font = UIFont.systemFontOfSize(60.0)
             let attr = [NSUnderlineStyleAttributeName : NSUnderlineStyle.StyleSingle.rawValue]
             emotionLabel.attributedText = NSAttributedString.init(string: self.emotions[i], attributes: attr)
+            emotionLabel.center = CGPointMake(width / 2.0, 200.0)
+            emotionLabel.textAlignment = NSTextAlignment.Center
+            emotionLabel.backgroundColor = UIColor.whiteColor()
             emotionPage.addSubview(emotionLabel)
         }
         
         self.emotionPages.contentSize = CGSizeMake(width * CGFloat(self.emotions.count), height)
     }
-
     
+    @IBAction func showList(sender: AnyObject) {
+        let page:Int = Int(self.emotionPages.contentOffset.x / self.emotionPages.frame.size.width)
+        
+        let alert = UIAlertController.init(title: "\(emotions[page])", message: "Showing results for \(emotions[page])", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction.init(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
