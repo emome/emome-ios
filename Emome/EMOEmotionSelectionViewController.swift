c41895344390525997aee0ff40e860a64af14414
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2015-09-28 10:46:45 -0400

    Add Emotion pages.

---
+++ Emome/EMOEmotionSelectionViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Emome
+//
+//  Created by Huai-Che Lu on 9/21/15.
+//  Copyright © 2015 Emome. All rights reserved.
+//
+
+import UIKit
+
+class EMOEmotionSelectionViewController: UIViewController {
+    
+    let emotions:[String] = ["happy", "angry", "lonely", "excited", "sad"]
+    let emotionColors:[UIColor] = [
+        UIColor.init(red: 253.0/255.0, green: 1.0, blue: 0.0, alpha: 1.0),
+        UIColor.init(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0),
+        UIColor.init(red: 204.0/255.0, green: 222.0/255.0, blue: 196.0/255.0, alpha: 1.0),
+        UIColor.init(red: 152.0/255.0, green: 1.0, blue: 0.0, alpha: 1.0),
+        UIColor.init(red: 66.0/255.0, green: 189.0/255.0, blue: 1.0, alpha: 1.0)
+    ]
+
+    @IBOutlet weak var emotionPages: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        addEmotionPages()
+    }
+    
+    func addEmotionPages() {
+        
+        let width = self.view.frame.width
+        let height = self.view.frame.height
+        
+        for i in 0..<self.emotions.count {
+            
+            
+            let frame = CGRectMake(width * CGFloat(i), 0.0, width, height)
+            let emotionPage = UIView.init(frame: frame)
+            emotionPage.backgroundColor = self.emotionColors[i]
+            self.emotionPages.addSubview(emotionPage)
+            
+            let emotionLabel = UILabel(frame: CGRectMake(32.0, 200.0, 320.0, 100.0))
+            emotionLabel.font = UIFont.systemFontOfSize(60.0)
+            let attr = [NSUnderlineStyleAttributeName : NSUnderlineStyle.StyleSingle.rawValue]
+            emotionLabel.attributedText = NSAttributedString.init(string: self.emotions[i], attributes: attr)
+            emotionPage.addSubview(emotionLabel)
+        }
+        
+        self.emotionPages.contentSize = CGSizeMake(width * CGFloat(self.emotions.count), height)
+    }
+
+    
+}
+
