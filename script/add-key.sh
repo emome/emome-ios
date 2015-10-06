6ea9fb6167ea907223354f3078dc50cc32ed687b
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2015-10-06 17:33:07 -0400

    Fix path problem in add-key.sh.

--- script/add-key.sh
+++ script/add-key.sh
@@ -8,10 +8,10 @@ if [[ "$TRAVIS_BRANCH" != "master" ]]; then
   exit 0
 fi
 
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in cert/developer.cer.enc -d -a -out cert/developer.cer
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in cert/developer.cer.enc -d -a -out cert/developer.cer
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ./script/cert/developer.cer.enc -d -a -out cert/developer.cer
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ./script/cert/developer.cer.enc -d -a -out cert/developer.cer
 
 security create-keychain -p travis ios-build.keychain
-security import cert/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import cert/developer.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import cert/developer.p12 -k ~/Library/Keychains/ios-build.keychain -P $IOS_KEY_PASSWORD -T /usr/bin/codesign
+security import ./script/cert/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./script/cert/developer.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./script/cert/developer.p12 -k ~/Library/Keychains/ios-build.keychain -P $IOS_KEY_PASSWORD -T /usr/bin/codesign
